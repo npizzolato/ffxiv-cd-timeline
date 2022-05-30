6cf9d5edafb139bef7fc0ff6abb1dc1db82e68e9
Nick Pizzolato <npizzolato@gmail.com>
2022-05-30 10:46:04 -0700

    Add checkboxes for the job timeline in detailed view

--- Data/Job.cs
+++ Data/Job.cs
@@ -2,7 +2,7 @@ public class Job
 {
     public string Name { get; set; }
 
-    public List<JobAbility> Abilities { get; set; }
+    public List<JobAbility> Abilities { get; set; } = new List<JobAbility>();
 
-    public List<JobTimelineEntry> AbilityTimeline { get; set; }
+    public List<JobTimelineEntry> AbilityTimeline { get; set; } = new List<JobTimelineEntry>();
 }
