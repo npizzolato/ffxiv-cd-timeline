d695efe4b4ae105969d882a4ae6bba058d24f558
Nick Pizzolato <npizzolato@gmail.com>
2022-06-05 11:39:17 -0700

    Encrypt saved mit plan

--- Providers/TimelineSaver.cs
+++ Providers/TimelineSaver.cs
@@ -1,3 +1,9 @@
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Web;
+using ICSharpCode.SharpZipLib.GZip;
 using Newtonsoft.Json;
 
 public class TimelineSaver
@@ -5,14 +11,50 @@ public class TimelineSaver
     public string SaveTimeline(MitigationTimeline timeline)
     {
         string timelineJson = JsonConvert.SerializeObject(timeline);
-        byte[] data = System.Text.Encoding.UTF8.GetBytes(timelineJson);
-        return System.Convert.ToBase64String(data);
+        byte[] compressedData = Zip(timelineJson);
+        string base64Encoded = System.Convert.ToBase64String(compressedData);
+        return HttpUtility.UrlEncode(base64Encoded);
     }
 
     public MitigationTimeline LoadTimeline(string savedTimeline)
     {
+        Console.WriteLine($"Loading {savedTimeline}");
+        //string decoded = HttpUtility.UrlDecode(savedTimeline);
         byte[] data = System.Convert.FromBase64String(savedTimeline);
-        string base64Decoded = System.Text.ASCIIEncoding.ASCII.GetString(data);
-        return JsonConvert.DeserializeObject<MitigationTimeline>(base64Decoded);
+        string uncompressed = Unzip(data);
+        //string base64Decoded = System.Text.ASCIIEncoding.ASCII.GetString(data);
+        return JsonConvert.DeserializeObject<MitigationTimeline>(uncompressed);
+    }
+
+    public static byte[] Zip(string str) {
+        var bytes = Encoding.UTF8.GetBytes(str);
+
+        using (var msi = new MemoryStream(bytes))
+        using (var mso = new MemoryStream()) {
+            GZip.Compress(msi, mso, true);
+
+            return mso.ToArray();
+        }
+    }
+
+    public static string Unzip(byte[] bytes) {
+        using (var msi = new MemoryStream(bytes))
+        using (var mso = new MemoryStream()) {
+            GZip.Decompress(msi, mso, true);
+
+            return Encoding.UTF8.GetString(mso.ToArray());
+        }
+    }
+    
+    private static void CopyTo(Stream src, Stream dest) 
+    {
+        byte[] bytes = new byte[4096];
+
+        int cnt;
+
+        while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0) 
+        {
+            dest.Write(bytes, 0, cnt);
+        }
     }
 }
